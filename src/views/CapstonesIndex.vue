bd1859cb63fd3cd3cec9784e264afada306523fb
moshesegelman <moshesegelman41@gmail.com>
2020-07-23 11:25:06 -0700

    adds cards on index page

--- src/views/CapstonesIndex.vue
+++ src/views/CapstonesIndex.vue
@@ -2,38 +2,49 @@
   <div class="capstones-index">
     <h1>{{ message }}</h1>
 
-    <div v-for="capstone in capstones">
-      <h1>{{ capstone.name }}</h1>
-      <img v-bind:src="capstone.screenshot_url" alt="">
-      <router-link v-bind:to="capstone.url">View Capstone Project</router-link>
+    <div class="container">
+      <div class="row row-cols-3">
+        <div v-for="capstone in capstones" class="col">
+        <div class="card-deck" >
+          <div class="card">
+            <img v-bind:src="capstone.screenshot_url" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{ capstone.name }}</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div> 
+      </div>
     </div>
+
+     
   </div>
 </template>
 
 <style>
+img {
+  height: 15em;
+}
 </style>
 
 <script>
-// import axios from "axios";
+import axios from "axios";
 export default {
-  data: function() {
+  data: function () {
     return {
       message: "Capstone Projects",
-      capstones: [
-        {
-          name: "rs-track",
-          screenshot_url:
-            "https://www.apimages.com/Images/Ap_Creative_Stock_Header.jpg"
-        }
-      ]
+      capstones: [],
     };
   },
-  created: function() {
-    axios.get("/api/capstones").then(response => {
+  created: function () {
+    axios.get("/api/capstones").then((response) => {
       console.log("All Capstones", response.data);
       this.capstones = response.data;
     });
+    console.log("index");
   },
-  methods: {}
+  methods: {},
 };
 </script>
